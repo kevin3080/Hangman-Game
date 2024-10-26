4293aa4500a536c4adb6a1c4a3386c4c7913375d
Kevin <kevinpernia29@gmail.com>
2024-10-26 13:45:14 -0500

    building structure

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import Keyboard from './components/keyboard/Keyboard.vue';
+import Header from './components/header/Header.vue';
+</script>
+
+<template>
+  <Header />
+  <main>
+    <div class="container-game">
+      <div class="game"><p>aqui va el juego</p></div>
+    </div>
+    <Keyboard />
+    
+  </main>
+</template>
+
+<style scoped>
+
+
+
+
+  .container-game {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+  .game {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 400px;
+    width: 80%;
+    border-radius: 25px;
+  }
+
+  .game::before{
+    z-index: -1;
+    content: "";
+    display: block;
+    position: absolute;
+    border-radius: 25px;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    backdrop-filter: blur(90px);
+    opacity: 0.7;
+  }
+
+  @media (min-width: 1024px) {
+    .game {
+      max-width: 1200px;
+    }
+  }
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import {
+  FlFilledNavigation,
+  BsArrowLeftCircleFill,
+} from "@kalimahapps/vue-icons";
+import { ref } from "vue";
+
+const menuIsOpen = ref(false);
+const menuOptionSelected = ref("Names");
+
+const openMenu = () => {
+  menuIsOpen.value = true;
+};
+
+const closeMenu = () => {
+  menuIsOpen.value = false;
+};
+</script>
+<template>
+  <header>
+    <div>
+      <nav class="navbar">
+        <div class="hamburger-icon">
+          <FlFilledNavigation class="icon" @click="openMenu" />
+        </div>
+        <div class="dropdown-menu" :class="{ open: menuIsOpen }">
+          <div class="header-menu">
+            <button class="close-button" @click="closeMenu">
+              <BsArrowLeftCircleFill class="icon" />
+            </button>
+          </div>
+          <ul class="list-menu">
+            <li @click="menuOptionSelected = 'Names'">Names</li>
+            <li @click="menuOptionSelected = 'Countries'">Countries</li>
+            <li @click="menuOptionSelected = 'Colros'">Colros</li>
+            <li @click="menuOptionSelected = 'Objects'">Objects</li>
+          </ul>
+        </div>
+        <div class="title-container">
+          <h1 class="title">{{ menuOptionSelected }}</h1>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+.navbar {
+  position: relative;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 60px;
+  color: #fff;
+  padding: 0 20px;
+}
+
+.hamburger-icon {
+  font-size: 30px;
+  cursor: pointer;
+}
+
+.icon {
+  display: block;
+}
+
+.title-container {
+  z-index: 1;
+  margin-left: 2rem;
+}
+
+.dropdown-menu {
+  position: fixed;
+  top: 0;
+  left: -310px;
+  min-width: 310px;
+  height: 100%;
+  background-color: #333;
+  color: #fff;
+  transition: transform 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  z-index: 1;
+}
+
+.dropdown-menu.open {
+  transform: translateX(310px);
+}
+
+.header-menu {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.close-button {
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.list-menu {
+  list-style: none;
+  padding: 0;
+  margin-top: 30px;
+}
+
+.list-menu li {
+  padding: 0.4rem 0 0.4rem 1rem;
+  margin: 0.6rem 0;
+  border-radius: 25px;
+  transition: background-color 0.2s ease;
+}
+
+.list-menu li:hover {
+  background-color: #fff;
+  color: #333;
+  cursor: pointer;
+}
+</style>
